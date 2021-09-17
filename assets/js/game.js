5828dc7d193ce2e35bb063ceec2d377428d5f35b
Tyler Norman <tnorman600@gmail.com>
2021-09-17 08:37:50 -0600

    added basic gameplay function for a single round of battle

---
+++ assets/js/game.js
@@ -0,0 +1,52 @@
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+
+    // player attacks
+    enemyHealth = enemyHealth - playerAttack;
+
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+
+    // check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    } 
+
+    else {
+        window.alert(enemyName + " has " + enemyHealth + " health left.");
+    }
+
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+
+    //enemy attacks
+    playerHealth = playerHealth - enemyAttack;
+
+    // check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+
+    else {
+        window.alert(playerName + " has " + playerHealth + " health left.");
+    }
+
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+
+    // Log a resulting message to the console so we know that it worked.
+};
+
+fight();
