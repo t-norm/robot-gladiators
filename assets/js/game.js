a5d95cd42a4a06338a3f8d027b8822d474ad6fb9
Tyler Norman <tnorman600@gmail.com>
2021-09-20 07:16:35 -0600

    fixed bug to prompt player if they entered an invalid/null robot name

--- assets/js/game.js
+++ assets/js/game.js
@@ -122,6 +122,17 @@ var randomNumber = function(min, max) {
     return value;
 };
 
+var getPlayerName = function() {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+    
+    console.log("Your robot's name is " + name);
+    return name;
+};
+
 var playerInfo = {
     name: window.prompt("What is your robot's name?"),
     health: 100,
